c91d521f7dfc7b6aef3a38a7ce27e79641ac9832
Anubhav Shukla <76528481+mightyanubhav@users.noreply.github.com>
2022-05-17 08:11:37 +0530

    Add files via upload

---
+++ 17--05pg2/Solution.java
@@ -0,0 +1,43 @@
+package newPackRevision;
+import java.util.*;
+
+class solve
+{
+	static String bnk[] = {"","","ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ"};
+    static ArrayList <String> possibleWords(int a[], int N)
+    {
+        // your code here 
+    	String val = "";
+    	for(int x : a)
+    		val = val+x;
+    	
+       ArrayList<String> ans= kpc(val);
+       return ans;
+        
+    }
+    static ArrayList<String> kpc(String st)
+    {
+    	if(st.length()==0) {
+    		ArrayList<String> y = new ArrayList<>();
+    		y.add("");
+    		return y;
+    	}
+    	
+    	char ch = st.charAt(0);
+    	String rS = st.substring(1);
+    	
+    	ArrayList<String> chSol = kpc(rS);
+    	ArrayList<String> sol = new ArrayList<>();
+    	
+    	String firstSt = bnk[ch-'0'];
+    	for(int i = 0 ;i<firstSt.length(); i++)
+    	{
+    		char c = firstSt.charAt(i);
+    		for(String x : chSol)
+    			sol.add(c + x);
+    	}
+    	
+    	return sol;
+    }
+}
+
